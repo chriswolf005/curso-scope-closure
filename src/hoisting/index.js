e1e879334c40ed54ca183e2a64bb72711169f149
Chris <christophersanchezwolf005@gmail.com>
2023-02-25 23:53:16 -0400

    Se agregaron la carpeta hoisting para explicar ese concepto y debug

---
+++ src/hoisting/index.js
@@ -0,0 +1,15 @@
+
+/* var nameOfDog;//undefined
+console.log(nameOfDog);
+var nameOfDog='Elmo';
+console.log(nameOfDog); */
+
+
+var elmo;// undefiend
+nameOfDog();
+
+function nameOfDog(){
+    console.log(`El mejor perrito es ${elmo}`);
+}
+
+var elmo='elmito';
